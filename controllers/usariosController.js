b2904a4856b97d5f47fc0101904221a9a74700fd
RonierisonBraz <ronierison18@gmail.com>
2021-04-05 11:58:47 -0300

    criado arquivo de usuarios dentro do model

--- controllers/usariosController.js
+++ controllers/usariosController.js
@@ -1,7 +1,8 @@
 const usuariosController = {
     index: (req, res) => {
            res.send('estou exibindo essa mensagem atraves do controller !')
-    }
+    
+        }
 }
 
 module.exports = usuariosController;
---
+++ models/usuarios.js
@@ -0,0 +1,7 @@
+const usuarios = {
+    listarUsusarios: () => {
+        return ['ana', 'geisiane', 'luiz', 'mateus'];
+    }
+}
+
+module.exports = usuarios;
